dbb4a264e8e0eb1058668d82d624389055514b9e
dragonwarriormaster <dragonwarriormaster@outlook.com>
2017-09-11 14:20:41 +0800

    use Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,20 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
 });
